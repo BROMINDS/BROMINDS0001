82d0a5808a85de8800733c59a7856c986a871f66
Andy Jhonson <andy645jh@gmail.com>
2018-04-22 20:00:11 -0500

    Se eliminaro carpetas innecesarias

---
+++ BROMINDS0001/Assets/Scripts/DetectorCapsule.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectorCapsule : MonoBehaviour {
+
+	public Animator anim;
+
+	void OnCollisionEnter(Collision col)
+	{
+		Debug.Log ("OnCollisionEnter: "+col.gameObject.name);
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		Debug.Log ("OnTriggerEnter: " + col.gameObject.name);
+		if (col.gameObject.tag == "Player") {
+			Player player = col.GetComponent<Player> ();
+			player.rigi.velocity = Vector3.zero;
+			player.transform.position = new Vector3(transform.position.x, player.transform.position.y,transform.position.z);
+			player.transform.LookAt (transform.right);
+			player.canMove = false;
+			gameObject.SetActive (false);
+			anim.Play ("closeDoor");
+		}
+	}
+}
---
+++ BROMINDS0001/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public Rigidbody rigi;
+	public Animator anim;
+	public float vel = 5;
+	public Camera cam;
+	public bool canMove = true;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!canMove)
+			return;
+		
+		float inputX = Input.GetAxisRaw("Horizontal");
+		float inputZ = Input.GetAxisRaw("Vertical");
+
+		float velLateral = inputX * Time.deltaTime * 100000 * vel; 
+		float velForward = inputZ * Time.deltaTime * 100000 * vel; 
+
+		Vector3 mousePos = cam.ScreenToViewportPoint (Input.mousePosition);
+		//cam.transform.localEulerAngles = cam.transform.localEulerAngles + Vector3.up * mousePos.normalized.x * 0.05f;
+		float mouseX = Input.GetAxisRaw("Mouse X");
+
+
+		rigi.AddForce (cam.transform.forward.normalized * velForward);
+		rigi.AddForce (cam.transform.right.normalized * velLateral);
+		//Debug.Log ("Update velX: "+velX+ " -- velZ: "+velZ);
+		//Debug.Log ("Update mousePos: "+mousePos.normalized.x);
+	}
+}
