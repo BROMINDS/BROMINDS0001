863fecb9b0c632f80bc66940bb6b0ab96d0b6ff0
Andy Jhonson <andy645jh@gmail.com>
2018-04-22 19:44:02 -0500

    Se agrega carpeta para recursos del proyecto

---
+++ BROMINDS0001/Assets/ResourcesExtras/ThirdPersonShooter/NOTAS.js
@@ -0,0 +1,44 @@
+﻿/*
+
+INSTRUCCIONES
+
+Teclado
+W,A,S,D       para movernos
+(W,A,S,D)+Q   esprintar
+SPACE         si estamos cerca de una pared con tag "Escalar"
+E             para coger arma
+Z             para cambiar la distancia entre la camera y el player
+R             para recargar arma
+
+Raton
+ClicIzquierdo(Fire1)    disparar/ataque dependiendo de si tenemos o no tenemos arma
+ClicDerecho(Fire2)      apuntar
+ClicMedio(Rueda)(Fire3) cambiar de arma entre principal y secundaria
+
+
+TUTORIALES UNITY
+
+  1-Time.deltaTime
+    https://unity3d.com/es/learn/tutorials/topics/scripting/delta-time
+
+  2-GetAxis
+    https://unity3d.com/es/learn/tutorials/topics/scripting/getaxis
+
+  3-Vector3 Direccion
+    https://docs.unity3d.com/ScriptReference/Vector3.html
+
+  4-Atributos
+    https://unity3d.com/es/learn/tutorials/topics/scripting/attributes
+
+  5-Mathf.SmoothDampAngle
+    https://docs.unity3d.com/ScriptReference/Mathf.SmoothDampAngle.html
+
+  6-Mathf.Repeat
+    https://docs.unity3d.com/ScriptReference/Mathf.Repeat.html
+
+OTROS VIDEOS
+
+  1-TheLastOfUs animaciones
+    https://www.youtube.com/watch?v=ZdEPZ1VaP6k
+    https://www.youtube.com/watch?v=aDVSnaWc5HU
+*/
